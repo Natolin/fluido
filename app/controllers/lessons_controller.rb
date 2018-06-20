eee44b12a80cb7fbd7d059c6e43e47afa08b74f6
Jesse Davis <jdavis93@live.com>
2018-06-20 12:25:06 +0200

    new and create methods setup

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,9 +9,17 @@ class LessonsController < ApplicationController
   end
 
   def new
+    @lesson = Lesson.new
   end
 
   def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.user = current_user
+    if @lesson.save
+      redirect_to lesson_path(@lesson)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -29,5 +37,9 @@ class LessonsController < ApplicationController
 
   def set_lesson
     @lesson = Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:description, :price, :language_id, :user_id )
   end
 end
