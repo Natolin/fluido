e2162f63ca9fd50c543eed80bf70b52aa2de1064
Jesse Davis <jdavis93@live.com>
2018-06-20 14:32:16 +0200

    users seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,10 +32,10 @@ puts "Creating seeds..."
 
 
 
-syd = User.create(first_name: "Syd", last_name: "Thomas", username: "MissAmerica", country: "UsaBitch",email: "s@s.com", password: "ssssss")
-jess = User.create(first_name: "Jesse", last_name: "Davis", username: "MisterMontana", country: "UsaDude",email: "j@j.com", password: "jjjjjj")
-pit = User.create(first_name: "Piotr", last_name: "Godzinski", username: "DrunkCaptain", country: "Poland",email: "p@p.com", password: "pppppp")
-chris = User.create(first_name: "Chris", last_name: "Philipps", username: "Fluidish", country: "Germany",email: "c@c.com", password: "cccccc")
+syd = User.create(first_name: "Syd", last_name: "Thomas", username: "MissAmerica", country: "UsaBitch",email: "s@s.com", password: "ssssss", description: 'hello everyboody, im nice and words and stuff nine ten', average_rating: 3.4, photo: image_tag ("babe.jpeg") )
+jess = User.create(first_name: "Jesse", last_name: "Davis", username: "MisterMontana", country: "UsaDude",email: "j@j.com", password: "jjjjjj", description: 'hello everyboody, im nice and words and stuff nine ten', average_rating: 5, image_tag ("gator.jpeg"))
+pit = User.create(first_name: "Piotr", last_name: "Godzinski", username: "DrunkCaptain", country: "Poland",email: "p@p.com", password: "pppppp", description: 'hello everyboody, im nice and words and stuff nine ten', average_rating: 2.9, image_tag ("kfc.jpeg"))
+chris = User.create(first_name: "Chris", last_name: "Philipps", username: "Fluidish", country: "Germany",email: "c@c.com", password: "cccccc", description: 'hello everyboody, im nice and words and stuff nine ten', average_rating: 4.5, image_tag ("shark.jpeg"))
 users = User.count
 puts "#{users} users created"
 
