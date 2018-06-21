1a9788d981425bd53e0efecc6445dcf5a3dc2581
Piotr Godzinski <hello@piotrgodzinski.com>
2018-06-21 16:49:12 +0200

    calendar aded to the dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,11 @@ class Booking < ApplicationRecord
   validates :end_date, presence: true
 
 
+
+
+  def start_time
+        self.start_date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
   # def change_status(new_status)
   #   if new_status == "pending" || "accepted" || "declined"
   #     self.status = new_status
