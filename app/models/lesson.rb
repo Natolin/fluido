997838f880d1348cbd829d81e4d5a1ed1a14f40c
Jesse Davis <jdavis93@live.com>
2018-06-21 11:10:01 +0200

    Global lesson search by language works in console

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,15 @@ class Lesson < ApplicationRecord
 
   validates :description, presence: true
   validates :price, presence: true
+
+  include PgSearch
+
+  pg_search_scope :global_search,
+    against: [ :id],
+    associated_against: {
+      language: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
