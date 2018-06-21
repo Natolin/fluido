4680e1d5233c70b96d86dc1b8bf92c1ac90ffd5d
Sydney Thomas <sydneyathomas@gmail.com>
2018-06-21 12:42:40 +0200

    convo#create action fixes so that it will not create a new convo if one b/t the 2 users already exists

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -11,15 +11,17 @@ class ConversationsController < ApplicationController
 
   def create # the user that you want to message is passed to this method
     user = User.find(params[:user_id].to_i)
-    convo = current_user.conversations.find do |conv|
+    my_id = current_user.id
+    myself = User.find(my_id)
+    convo = user.conversations.find do |conv|
       user_ids = conv.subscriptions.pluck(:user_id)
-      user_ids.include?(current_user.id) && user_ids.include?(user.id)
+      user_ids.include?(myself.id) && user_ids.include?(user.id)
     end
-    
+
     if convo.nil? 
       convo = Conversation.create
       other_sub = Subscription.create(user_id: user.id, conversation_id: convo.id)
-      my_sub = Subscription.create(user_id: current_user.id, conversation_id: convo.id)
+      my_sub = Subscription.create(user_id: myself.id, conversation_id: convo.id)
     end
    redirect_to conversation_path(convo)
    end
@@ -29,27 +31,6 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:id])
     @message = Message.new
 
-    @list = []
-    @other = []
-
-    # get all subs I have
-    @my_subs = Subscription.where(user_id: current_user.id)
-
-    # for each of my subscriptions
-    @my_subs.each do |sub|
-      # extract the convo using the id stored in the sub
-      @convo = Conversation.find(sub.conversation_id)
-
-      # extract only subs that have this convo.id (>= 2)
-      @both = Subscription.where(conversation_id: @convo.id)
-
-      #this will be an array of two
-      @other_subscription = @both.delete(Subscription.find_by(user_id: current_user.id))
-
-      @list << @convo
-      @other << @other_subscription
-
-    end
     
   end
 
