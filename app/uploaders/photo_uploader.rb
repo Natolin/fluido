ea6ffba5a39596aa01d40724d46c808cd1485ee8
Piotr Godzinski <hello@piotrgodzinski.com>
2018-06-22 14:28:46 +0200

    dashboard css fixes and default photo for user profile set

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -19,8 +19,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  
-  "http://res.cloudinary.com/df65oi2rs/image/upload/v1528997504/compass.jpg"
+
+  "http://res.cloudinary.com/df65oi2rs/image/upload/v1529668788/default_img.png"
   end
 
   # Process files as they are uploaded:
