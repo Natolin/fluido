413b81bf2c93f7683232064ff27432cb43afc54a
Sydney Thomas <sydneyathomas@gmail.com>
2018-06-25 12:30:35 +0200

    ajax for current user

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,21 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
-  validates :content, presence: true
-  validates :conversation_id, presence: true
-  validates :user_id, presence: true
-  
+  validates :content, presence: true, allow_blank: false
+
+
+  def from? (some_user)
+    user == some_user
+  end
+
+  def broadcast_message
+    ActionCable.server.broadcast("conversation_#{conversation.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/message",
+        locals: { message: self, user_is_messages_author: false }
+      ),
+      current_user_id: user.id
+    })
+  end
+
 end
