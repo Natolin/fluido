0693576c2db658b99491ac2f4643f80887399b6e
Jesse Davis <jdavis93@live.com>
2018-06-26 13:33:10 +0200

    Users now redirected to Dashboard after sign/in/up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   #   update_attrs = [:password, :password_confirmation, :current_password]
   #   devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   # end
-
+  def after_sign_in_path_for(resource)
+    dashboard_path(current_user)
+  end
 end
 
 
